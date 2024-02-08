349b4c78cffabc1d501e7702ffd45d8aaae9db0b
Angel Yan <angelyan96@gmail.com>
2024-02-08 18:19:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# canvaskit-wasm
+
+This repository contains a custom build of [`canvaskit-wasm`](https://www.npmjs.com/package/canvaskit-wasm). It is based
+on the basic canvaskit build with the following changes:
+
+* Adds JPEG encoding support.
+
+## Instructions
+
+The package is built using the `build.sh` script. To build a new canvaskit bundle you can edit this script as needed and
+run it. For example, you can change the `CANVASKIT_VERSION` variable to build a new version or change the compilation
+flags to enable/disable features. After building, commit all changes and push.
---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+CANVASKIT_VERSION=0.38.2
+
+repo_root=$(git rev-parse --show-toplevel)
+
+# Download canvaskit
+canvaskit_tag="canvaskit/$CANVASKIT_VERSION"
+if [ ! -d skia ]; then
+  # Clone the repo if it doesn't exist
+  git clone --depth 1 --branch "$canvaskit_tag" https://github.com/google/skia.git
+else
+  # Checkout the tag if the repo was already cloned
+  cd skia
+  git fetch origin +refs/tags/"$canvaskit_tag"
+  git checkout "$canvaskit_tag"
+  git reset --hard "$canvaskit_tag"
+  cd -
+fi
+
+# Install dependencies
+cd skia
+tools/git-sync-deps
+bin/fetch-ninja
+
+# Compile canvaskit
+cd modules/canvaskit
+./compile.sh release \
+  no_skottie \
+  no_sksl_trace \
+  no_alias_font \
+  no_effects_deserialization \
+  no_encode_webp \
+  legacy_draw_vertices \
+  no_embedded_font
+
+# Write npm package files
+cd "$repo_root"
+rm -rf bin
+mkdir bin
+cp skia/out/canvaskit_wasm/canvaskit.js   bin
+cp skia/out/canvaskit_wasm/canvaskit.wasm bin
+cp -r skia/modules/canvaskit/npm_build/types .
+# Copy the package.json leaving only the exports for the basic canvaskit build
+jq < skia/modules/canvaskit/npm_build/package.json >package.json \
+  '.exports |= {".", "./bin/canvaskit", "./bin/canvaskit.js", "./bin/canvaskit.wasm"}'
